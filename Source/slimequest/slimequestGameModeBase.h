52f0b02d6f75442f45f435c05c15cda2661e8d6c
Nathaniel Richards <nthnlrichards@gmail.com>
2023-03-23 01:39:29 -0400

    Initial commit

---
+++ Source/slimequest/slimequestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "slimequestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLIMEQUEST_API AslimequestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
